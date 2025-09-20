2fdc62ebdb5b093ebf089d121a0c751f76ec20ec
anishsingh234 <squirtlewartotleblastoise28@gmail.com>
2025-09-20 16:54:31 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,38 @@ const nextConfig = {
       "camo.githubusercontent.com",
       "github-readme-stats.vercel.app",
     ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github-readme-stats.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'streak-stats.demolab.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'skillicons.dev',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ghchart.rshah.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github-profile-trophy.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
